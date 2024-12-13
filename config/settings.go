ce14e44e57f92824596e82d22750286bd487d50d
anomit ghosh <anomit.ghosh@gmail.com>
2024-12-13 20:06:15 +0530

    chore: remove data market in request config field

--- config/settings.go
+++ config/settings.go
@@ -18,15 +18,10 @@ type Settings struct {
 	RelayerPrivateKey      string
 	SlackReportingUrl      string
 	DataMarketAddress      string
-	DataMarketInRequest    bool
 	RedisDB                string
 }
 
 func LoadConfig() {
-	dataMarketInRequest, err := strconv.ParseBool(getEnv("DATA_MARKET_IN_REQUEST", "false"))
-	if err != nil {
-		log.Fatalf("Invalid value for DATA_MARKET_IN_REQUEST: %v", err)
-	}
 	config := Settings{
 		ClientUrl:              getEnv("PROST_RPC_URL", ""),
 		ContractAddress:        getEnv("PROTOCOL_STATE_CONTRACT", ""),
@@ -36,8 +31,7 @@ func LoadConfig() {
 		RelayerPrivateKey:      getEnv("RELAYER_PRIVATE_KEY", ""),
 		SlackReportingUrl:      getEnv("SLACK_REPORTING_URL", ""),
 		DataMarketAddress:      getEnv("DATA_MARKET_ADDRESS", ""),
-		RedisDB:                getEnv("REDIS_DB", ""),
-		DataMarketInRequest:    dataMarketInRequest,
+		RedisDB:                getEnv("REDIS_DB", "")
 	}
 	
 
