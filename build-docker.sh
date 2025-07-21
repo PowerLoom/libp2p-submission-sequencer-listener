f6cd51b543f6e8516275876b394cb0b6f495cc81
anomit ghosh <anomit.ghosh@gmail.com>
2025-07-21 15:57:26 +0530

    chore: support both docker-compose and docker compose V2

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker-compose build
+
+# Check if docker compose V2 is available
+if docker compose version &> /dev/null; then
+    docker compose build
+else
+    docker-compose build
+fi
