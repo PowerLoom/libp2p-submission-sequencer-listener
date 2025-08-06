7ac6786a55c7e1e74b9c2c19bb5a750dc4a80a13
anomit ghosh <anomit.ghosh@gmail.com>
2025-08-06 18:48:49 +0530

    feat: add logging configuration and persistence to Docker setup

--- pkgs/utils/logger.go
+++ pkgs/utils/logger.go
@@ -1,34 +1,62 @@
 package utils
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"github.com/sirupsen/logrus/hooks/writer"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func InitLogger() {
-	log.SetOutput(io.Discard) // Send all logs to nowhere by default
+	// Check if LOG_FILE environment variable is set
+	logFile := os.Getenv("LOG_FILE")
+	if logFile != "" {
+		// Ensure log directory exists
+		logDir := filepath.Dir(logFile)
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			fmt.Printf("Failed to create log directory: %v\n", err)
+			// Fall back to stdout/stderr
+			logFile = ""
+		} else {
+			// Open log file
+			file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			if err != nil {
+				fmt.Printf("Failed to open log file: %v\n", err)
+				// Fall back to stdout/stderr
+				logFile = ""
+			} else {
+				// Write to both file and stdout/stderr
+				log.SetOutput(io.MultiWriter(file, os.Stdout))
+			}
+		}
+	}
+
+	// If no log file or failed to open, use default behavior
+	if logFile == "" {
+		log.SetOutput(io.Discard) // Send all logs to nowhere by default
+
+		log.AddHook(&writer.Hook{ // Send logs with level higher than warning to stderr
+			Writer: os.Stderr,
+			LogLevels: []log.Level{
+				log.PanicLevel,
+				log.FatalLevel,
+				log.ErrorLevel,
+				log.WarnLevel,
+			},
+		})
+		log.AddHook(&writer.Hook{ // Send info and debug logs to stdout
+			Writer: os.Stdout,
+			LogLevels: []log.Level{
+				log.TraceLevel,
+				log.InfoLevel,
+				log.DebugLevel,
+			},
+		})
+	}
 
 	log.SetReportCaller(true)
-
-	log.AddHook(&writer.Hook{ // Send logs with level higher than warning to stderr
-		Writer: os.Stderr,
-		LogLevels: []log.Level{
-			log.PanicLevel,
-			log.FatalLevel,
-			log.ErrorLevel,
-			log.WarnLevel,
-		},
-	})
-	log.AddHook(&writer.Hook{ // Send info and debug logs to stdout
-		Writer: os.Stdout,
-		LogLevels: []log.Level{
-			log.TraceLevel,
-			log.InfoLevel,
-			log.DebugLevel,
-		},
-	})
 	log.SetLevel(log.DebugLevel)
 	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
 }
